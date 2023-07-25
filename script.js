f97dc5c651e7d3271ef0327700030a9447e40043
RichRich087 <>
2023-07-25 17:31:46 +0000

    added meta data, couple minor changes

--- script.js
+++ script.js
@@ -102,10 +102,10 @@ function popUp(title) {
         width: 600,
         padding: '3em',
         color: '#716add',
-        background: '#fff url(/images/trees.png)',
+        background: '#fff url(images/trees.png)',
         backdrop: `
             rgba(0,0,123,0.4)
-            url("/images/nyan-cat.gif")
+            url("images/nyan-cat.gif")
             left top
             no-repeat
         `
